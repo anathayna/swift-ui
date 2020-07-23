056dc6af2b7ce58459cf004f66423fdde2c4abbe
anathayna <anathaynafranca@gmail.com>
2020-07-23 12:46:24 -0300

    adding fructus project

---
+++ fructus/fructus/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  fructus
+//
+//  Created by Ana Thayna Franca on 23/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fructus/fructus/fructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fructusApp.swift
+//  fructus
+//
+//  Created by Ana Thayna Franca on 23/07/20.
+//
+
+import SwiftUI
+
+@main
+struct fructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
