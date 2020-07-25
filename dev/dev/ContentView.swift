c976c6a1c2419e1971d0dee5a11b41b71142d4b4
anathayna <anathaynafranca@gmail.com>
2020-07-25 03:47:15 -0300

    adding dev project

---
+++ dev/dev/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  dev
+//
+//  Created by Ana Thayna Franca on 25/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dev/dev/devApp.swift
@@ -0,0 +1,17 @@
+//
+//  devApp.swift
+//  dev
+//
+//  Created by Ana Thayna Franca on 25/07/20.
+//
+
+import SwiftUI
+
+@main
+struct devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
