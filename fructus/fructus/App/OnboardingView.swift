02dc85c4c7310c4fced4518b6341bc174c22e68e
anathayna <anathaynafranca@gmail.com>
2020-07-25 15:34:34 -0300

    fetch daa

--- fructus/fructus/App/OnboardingView.swift
+++ fructus/fructus/App/OnboardingView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    var fruits: [Fruit] = fruitsData
+    
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                //FruitCardView()
-                Text("Cards")
+            ForEach(fruits[0...12]) { item in
+                FruitCardView(fruit: item)
             }
         }
         .tabViewStyle(PageTabViewStyle())
@@ -22,6 +23,7 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
+            .previewDevice("IPhone SE")
     }
 }
